3cb893eb03ea70b4d36f926d62a021a2ffc96697
Henri Sellis <henri.sellis@gmail.com>
2020-11-20 20:52:19 +0200

    Now follow/unfollow also changes button text

--- homework3_vue_app/src/components/BrowseUsers.vue
+++ homework3_vue_app/src/components/BrowseUsers.vue
@@ -4,8 +4,8 @@
     <div class="main-container">
       <div v-for='profile in profiles' :key='profile.id'>
         <div class="profile">
-          <h2>{{ profile.firstname | capitalize }} {{ profile.lastname | capitalize }}</h2>
           <img :src="profile.avatar" :alt="profile.firstname + ' ' +  profile.lastname">
+          <h2>{{ profile.firstname | capitalize }} {{ profile.lastname | capitalize }}</h2>
           <button id="follow" class="follow-button" @click="toggleFollow($event.target)">Follow</button>
         </div>
       </div>
@@ -34,7 +34,11 @@ export default {
     toggleFollow: function (followButton) {
 
       followButton.classList.toggle("follow-button-followed");
-
+      if (followButton.innerHTML === "Follow"){
+        followButton.innerHTML = "Followed"
+      } else {
+        followButton.innerHTML = "Follow"
+      }
     }
   },
   filters: {
@@ -53,11 +57,42 @@ export default {
 
 <style scoped>
 
+.main-container {
+  width: 50%;
+  min-height: 100%;
+  margin: auto auto;
+  padding: 90px 15px 15px 15px;
+  background-color: #ffffff;
+  column-count: 2;
+  justify-content: center;
+}
+
+.profile {
+  width: 45%;
+  display: inline-block;
+  border: 1px solid #dedede;
+  border-radius: 5px;
+  text-align: center;
+  margin: 1%;
+}
+.profile img{
+  width: 75px;
+  height: 75px;
+  border-radius: 100%;
+  object-fit: cover;
+  object-position: top;
+  margin: 5px;
+}
+
+.profile h2{
+  font-size: 16px;
+}
+
 .follow-button{
   background-color: #82008f;
 }
 
-.follow-button-followed{
+.follow-button.followed{
   background-color: #ffffff;
   border: 1px solid #82008f;
   color: #82008f;
